878c8aee77d39ccc594040fc7f2c83f3394df550
Binyam6-wy <amearg9@gmail.com>
2026-03-13 13:55:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,39 @@ function post() {
     document.querySelector('input').value = "";
   }
 }
+// LOGIN FUNCTION
+function login() {
+
+  let username = document.getElementById("username").value;
+
+  if (username === "") return;
+
+  localStorage.setItem("nexifyUser", username);
+
+  window.location.href = "profile.html";
+}
+
+
+// SHOW USER ON PROFILE PAGE
+window.onload = function () {
+
+  let name = localStorage.getItem("nexifyUser");
+
+  if (document.getElementById("displayName")) {
+
+    if (name) {
+      document.getElementById("displayName").textContent = name;
+    } else {
+      document.getElementById("displayName").textContent = "Guest";
+    }
+  }
+};
+
+
+// LOGOUT FUNCTION
+function logout() {
+
+  localStorage.removeItem("nexifyUser");
+
+  window.location.href = "login.html";
+}
